0583e83ec95d8d7b2757e76691f2f2a2540af403
Jesko Karwasz <warhuhn@googlemail.com>
2014-10-18 20:59:44 +0200

    Implementes the render Method, added TODOs

--- src/main/java/de/warhuhn/java/web/spark/JtwigTemplateEngine.java
+++ src/main/java/de/warhuhn/java/web/spark/JtwigTemplateEngine.java
@@ -1,5 +1,12 @@
 package de.warhuhn.java.web.spark;
 
+import com.lyncode.jtwig.JtwigContext;
+import com.lyncode.jtwig.JtwigTemplate;
+import com.lyncode.jtwig.exception.ParseException;
+import com.lyncode.jtwig.exception.CompileException;
+import com.lyncode.jtwig.exception.RenderException;
+import com.lyncode.jtwig.resource.ClasspathJtwigResource;
+import org.eclipse.jetty.io.RuntimeIOException;
 import spark.ModelAndView;
 import spark.TemplateEngine;
 
@@ -7,8 +14,22 @@ import spark.TemplateEngine;
  * Created by suigintou on 18.10.2014.
  */
 public class JtwigTemplateEngine extends TemplateEngine {
+
+    // TODO: Needs a way to further configure Jtwig
+
     @Override
     public String render(ModelAndView modelAndView) {
-        return null;
+        JtwigTemplate template = new JtwigTemplate(new ClasspathJtwigResource("templates/" + modelAndView.getViewName()));
+
+        String result;
+
+        try {
+            // TODO: will break if the model is not a HashMap<String, Object>
+            result = template.output(new JtwigContext((com.lyncode.jtwig.JtwigModelMap) modelAndView.getModel()));
+        } catch (ParseException | CompileException | RenderException e) {
+            throw new RuntimeIOException(e);
+        }
+
+        return result;
     }
 }
