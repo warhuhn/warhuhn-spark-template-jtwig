e4ee49d84b2411fa11a42951aedf658a49ceec7d
Jesko Karwasz <warhuhn@googlemail.com>
2014-10-18 20:06:20 +0200

    Added class stub

---
+++ src/main/java/de/warhuhn/java/web/spark/JTwigTemplateEngine.java
@@ -0,0 +1,14 @@
+package de.warhuhn.java.web.spark;
+
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+/**
+ * Created by suigintou on 18.10.2014.
+ */
+public class JTwigTemplateEngine extends TemplateEngine {
+    @Override
+    public String render(ModelAndView modelAndView) {
+        return null;
+    }
+}
